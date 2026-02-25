a367726291a3707964940c29664398cbbd99f6c7
Christian Jay Anunciado <christianjay.anunciado@gmail.com>
2026-02-26 02:06:01 +0800

    feat: removed depracted partyTown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,9 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.christiananunciado.com",
-  integrations: [tailwind(), react(), sitemap(), partytown({
-    config: {
-      forward: ["dataLayer.push"],
-    },
-  })],
+  integrations: [tailwind(), react(), sitemap()],
 });
